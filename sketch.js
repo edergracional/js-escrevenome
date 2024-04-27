88226f4d55ec3f2c7227ce5d3828abc3dbd7fe14
edergracional <168239934+edergracional@users.noreply.github.com>
2024-04-27 10:57:27 -0300

    Adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(400, 400);
+    background("black");
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("red");
+    
+     if(mouseIsPressed) {
+        rect(mousex, mousey, 20, 35);
+    
+  }
+         
+        
+    }
